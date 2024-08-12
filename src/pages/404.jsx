ebf85b236ac2e2baa83f46186535186db7dd57ef
AhNode <jakijapanese@gmail.com>
2024-08-12 17:30:30 +0700

    rubah beberapa

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -6,7 +6,7 @@ export default function ErrorPage() {
 	return (
 		<div className="App flex justify-center min-h-screen items-center flex-col">
 			<h1 className="text-3xl font-bold">Oops!</h1>
-			<p className="my-2 text-xl">Sepertinya kamu nyasar di website ini</p>
+			<p className="my-2 text-xl">404, Sepertinya kamu nyasar</p>
 			<p>{error.statusText || error.message}</p>
 		</div>
 	);
